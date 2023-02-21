3b16b9c45532fd66b364f31ca7c6db21b57ed165
Theo <theo.gianella@proton.me>
2023-02-21 17:22:07 +0100

    Implemented event creation from users

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,32 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
+
   def index
     @events = Event.all
   end
 
-  def new; end
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+
+    if @event.save
+      flash[:success] = 'Votre événement a bien été créé'
+      redirect_to action: 'index'
+    else
+      flash[:error] = "Votre événement n'a pas pu être créé"
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def show; end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:start_date, :duration, :title, :description, :price, :location)
+  end
 end
